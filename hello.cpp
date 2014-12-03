d321b7539a022cd2840f2bf64fc64f66d42ca862
thl <wsthl19920901@126.com>
2014-12-03 21:21:22 +0800

    hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,14 @@
+/*************************************************************************
+	> File Name: hello.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: 2014年12月03日 星期三 21时11分13秒
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"hello world!"<<endl;
+    return 0;
+}
